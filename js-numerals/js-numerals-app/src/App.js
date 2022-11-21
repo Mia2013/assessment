c47e92b91418308060941d79413505c85c571fe9
Mia2013 <komlosnikolett@gmail.com>
2022-11-21 14:04:54 +0100

    add Title component

--- js-numerals/js-numerals-app/src/App.js
+++ js-numerals/js-numerals-app/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.scss';
+import "./App.scss";
+import { Grid } from "@mui/material";
+
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
-     
+      <Grid container>
+        <Grid item>
+          <Title title="Arabic Number Conversion Tool" />
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ js-numerals/js-numerals-app/src/components/Title.js
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/material";
+
+export default function Title({ title }) {
+  return <Typography variant="h3">{title}</Typography>;
+}
