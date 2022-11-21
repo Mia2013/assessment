6eeb2e3a8a623dd2ecdd48e018abd251f9a252a5
Mia2013 <komlosnikolett@gmail.com>
2022-11-21 13:55:08 +0100

    create-react-app, delete unnecessary codes and logos

---
+++ js-numerals/js-numerals-app/src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBK7G-yKUwcU3UuUPjyZcKMrFJ6HcT9niY",
+  authDomain: "arabic-number-conversion.firebaseapp.com",
+  projectId: "arabic-number-conversion",
+  storageBucket: "arabic-number-conversion.appspot.com",
+  messagingSenderId: "131364315656",
+  appId: "1:131364315656:web:a58a3e111ae7fe41e4e7bd"
+};
+
+const app = initializeApp(firebaseConfig);
