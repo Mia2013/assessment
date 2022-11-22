5385526900354ec273d56916c31aca42d386b778
Mia2013 <komlosnikolett@gmail.com>
2022-11-22 09:17:21 +0100

    add names of numbers

--- js-numerals/js-numerals-app/src/App.js
+++ js-numerals/js-numerals-app/src/App.js
@@ -5,6 +5,7 @@ import { Box, Grid, Typography } from "@mui/material";
 
 import Title from "./components/Title";
 import Input from "./components/Input";
+import GetNumberToEnglishWord from "./components/GetNumberToEnglishWord";
 
 const styles = {
   app: {
@@ -13,31 +14,33 @@ const styles = {
     backgroundSize: "100% 100%",
     backgroundPosition: "center",
     height: "100vh",
-    width: "100vw"
+    width: "100vw",
   },
-
 };
 
 function App() {
   const [number, setNumber] = useState({
     value: "",
-    formattedValue: ""
+    formattedValue: "",
   });
 
   return (
     <Box className="App" style={styles.app}>
       <Grid container>
-        <Grid item xs={12} sx={{textAlign: "center", mt: 10}}>
+        <Grid item xs={12} sx={{ textAlign: "center", mt: 10 }}>
           <Title title="Arabic Number Conversion Tool" />
         </Grid>
-        <Grid item xs={12} sx={{textAlign: "center", mt: 5, mb: 3}}>
-          <Typography variant="body2"> Convert a number to an English word representation</Typography>
+        <Grid item xs={12} sx={{ textAlign: "center", mt: 5, mb: 3 }}>
+          <Typography variant="body2">
+            {" "}
+            Convert a number to an English word representation
+          </Typography>
         </Grid>
-        <Grid item xs={10} sm={4}sx={{mx: "auto", textAlign: "center"}}>
+        <Grid item xs={10} sm={4} sx={{ mx: "auto", textAlign: "center" }}>
           <Input number={number} setNumber={setNumber} />
         </Grid>
-        <Grid item xs={12} sx={{textAlign: "center"}}>
-          <Typography > {number.value}</Typography>
+        <Grid item xs={10} sm={6} md={4} sx={{ mx: "auto", textAlign: "center" }}>
+          <GetNumberToEnglishWord />
         </Grid>
       </Grid>
     </Box>
---
+++ js-numerals/js-numerals-app/src/components/getNumberToEnglishWord.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Typography } from "@mui/material";
+
+export default function GetNumberToEnglishWord({ number }) {
+  const [word, setWord] = useState("");
+
+  const digitsUnderTwenty = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+  };
+
+  const tens = {
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+  };
+
+
+  const units = [
+      [1000000000000000, "quadrillion"],
+      [1000000000000, "trillion"],
+      [1000000000, "billion"],
+      [1000000, "million"],
+      [1000, "thousand"],
+      [1, ""],
+    ],
+
+  return <Typography>{word}</Typography>;
+}
