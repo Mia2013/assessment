68e48528a936237723ecb430a45d775b6db54fbf
Mia2013 <komlosnikolett@gmail.com>
2022-11-22 13:00:45 +0100

    add Alert to displayWord if number is NAN

--- js-numerals/js-numerals-app/src/components/DisplayWord.js
+++ js-numerals/js-numerals-app/src/components/DisplayWord.js
@@ -1,13 +1,32 @@
 import { useState, useEffect } from "react";
-import { Typography } from "@mui/material";
+import { Box, Typography, Alert } from "@mui/material";
 import { digitsToEnglish } from "./digitsToEnglish";
 
 export default function DisplayWord({ number }) {
   const [word, setWord] = useState("");
+  const [alert, setAlert] = useState(false);
 
   useEffect(() => {
-    if (number.value !== "") setWord(digitsToEnglish(number.value));
-  },[number.value]);
+    if (number.value !== "") {
+      if (typeof number.value === "number") {
+        setWord(digitsToEnglish(number.value));
+        setAlert(false);
+      } else {
+        setAlert(true);
+      }
+    }
+  }, [number.value]);
 
-  return <Typography>{word}</Typography>;
+  return (
+    <Box>
+      <Typography>{word}</Typography>
+      {alert ? (
+        <Alert severity="error">
+          {number.value} is not a number! Please enter a valid number!
+        </Alert>
+      ) : (
+        " "
+      )}
+    </Box>
+  );
 }
