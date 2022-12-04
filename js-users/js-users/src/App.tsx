e4a2a95fc582c842aef4b7f8040ae0725c1af0f3
Mia2013 <komlosnikolett@gmail.com>
2022-12-04 17:25:30 +0100

    get users from backend url

--- js-users/js-users/src/App.tsx
+++ js-users/js-users/src/App.tsx
@@ -1,8 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+interface User {
+  id: number;
+  last_name: string;
+  first_name: string;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+  url: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [alert, setAlert] = useState<string>("");
 
+  async function getUsers() {
+    const url = "https://assessment-users-backend.herokuapp.com";
+    try {
+      const response = await fetch(`${url}/users.json`);
+      const result = await response.json();
+      if (response.status === 200) {
+        setAlert("");
+        return setUsers(result);
+      }
+      throw new Error();
+    } catch (error) {
+      setAlert("Sorry, but something went wrong, please try again later");
+    }
+  }
 
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return <div className="App"></div>;
 }
