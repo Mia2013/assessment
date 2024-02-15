fef719d1e3187a90a32226f4a1b5d35a52145c6a
Nikolett Budavári <89342094+Mia2013@users.noreply.github.com>
2024-02-15 09:14:16 +0100

    Create api.ts

---
+++ js-users/js-users/api.ts
@@ -0,0 +1,17 @@
+interface QueryData {
+    [key: string]: string | number;
+  }
+  
+  export async function getData<T>(endpoint: string, query?: Partial<QueryData>) {
+    try {
+        const response = await axiosInstance.get(endpoint, {
+            params: query
+        });
+        if (response.status === 200) {
+            const result = response.data;
+            return result;
+        }
+    } catch (error) {
+        throw new Error(error)
+    }
+}
